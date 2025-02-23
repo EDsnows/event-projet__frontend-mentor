6c2e921a1990937031a7a886536deaf7910bd053
EDsnows <eduardo.castroneves19@gmail.com>
2025-02-23 20:40:42 -0300

    funcionalidade geral da imagem do formulário concluida

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,71 @@
-/*
-campo input da imagem deve ser alterado para adicionar 3 novos elementos
-    1- imagem do usuário que foi enviada (não fazer nada)
-
-    2- botão para remover a imagem
-        !! se o botão for clicado, a imagem que o usuário mandou deve ser removida e então voltar a como o campo input estava no seu estado "neutro"
-    
-    3- botão para trocar a imagem (terá o mesmo comportamento para caso o usuário esteja mandando a imagem pela primeira vez)
-*/
-
-// PRIMEIRO DESAFIO:
-// o campo input img deve receber a imagem que o usuário envio e substituir a imagem "placeholder", porém só deve ser feita essa alteração caso a imagem for do formato .JPG ou .PNG e também, se seu tamanho for de no máximo de 500KB. A imagem substituída deve receber o mesmo padrão de estilização aplicada na imagem "placeholder"
-
-const containerInputFile = document.querySelector('.container_input-avatar');
-const inputAvatarArea = document.querySelector('#upload-avatar');
+// variáveis
+const containerDragAndDrop = document.querySelector('.container_input-avatar');
+const inputFile = document.querySelector('#upload-avatar');
 const containerImageAvatar = document.querySelector('.container_image-upload');
 
+const textInfoActions = document.querySelector('.text-info-actions');
+const btnImages = document.querySelectorAll('button[type="button"]');
+
+const imgBackup = document.querySelector('.image-default');
+
+// funcionalidade da imagem do usuário
+
 function onFocus() {
-    containerInputFile.classList.add('active');
+    containerDragAndDrop.classList.add('active');
 }
 
 function onLeave() {
-    containerInputFile.classList.remove('active');
+    containerDragAndDrop.classList.remove('active');
 }
 
-containerInputFile.addEventListener('dragenter', onFocus);
-containerInputFile.addEventListener('drop', onLeave);
-containerInputFile.addEventListener('dragend', onLeave);
-containerInputFile.addEventListener('dragleave', onLeave);
+containerDragAndDrop.addEventListener('dragenter', onFocus);
+containerDragAndDrop.addEventListener('drop', onLeave);
+containerDragAndDrop.addEventListener('dragend', onLeave);
+containerDragAndDrop.addEventListener('dragleave', onLeave);
 
-inputAvatarArea.addEventListener('change', event => {
-    if (event.target.files.length > 0) {
-        const type = event.files[0].type;
-        const formats = ['image/jpg', 'image/png'];
-        if (!formats.includes(type)) {
-            alert('Esse formato não é permitido!');
-            return;
-        }
-
-        const newImg = document.createElement('img');
-        newImg.src = URL.createObjectURL(event.target.files[0]);
-        newImg.alt = 'New image avatar';
-
-        containerImageAvatar.replaceChild(newImg);
+inputFile.addEventListener('change', event => {
+    console.log('entrou no evento');
+    const file = event.target.files[0];
+    const type = file.type;
+    const formats = ['image/jpeg', 'image/png'];
+    if (!formats.includes(type)) {
+        alert('Esse formato não é permitido!');
+        return;
     }
+
+    const maxSizeInBytes = 500 * 1024;
+    if (file.size > maxSizeInBytes) {
+        alert('O arquivo precisa ser menos que 500KB');
+        return;
+    }
+
+    const newImg = document.createElement('img');
+    newImg.src = URL.createObjectURL(file);
+    newImg.alt = 'New image avatar';
+    newImg.setAttribute('class', 'new-img');
+
+    textInfoActions.classList.remove('active');
+    btnImages.forEach(button => {
+        button.classList.add('active');
+    });
+
+    containerImageAvatar.replaceChild(newImg, containerImageAvatar.children[0]);
+});
+
+btnImages[0].addEventListener('click', () => {
+    containerImageAvatar.replaceChild(
+        imgBackup,
+        containerImageAvatar.children[0]
+    );
+
+    btnImages.forEach(button => {
+        button.classList.remove('active');
+    });
+    textInfoActions.classList.add('active');
+
+    inputFile.value = '';
+});
+
+btnImages[1].addEventListener('click', () => {
+    inputFile.click();
 });
